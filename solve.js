f2e6108757c92071b7c31381a5302286777407a4
Ward Cunningham <ward@c2.com>
2021-03-29 14:37:44 -0700

    now html

--- solve.js
+++ solve.js
@@ -1,6 +1,9 @@
-addEventListener("fetch", (event) => {
-  const response = new Response(`Sudoku Solver Coming Soon\n\n${Object.keys(event).join("\n")}`, {
-    headers: { "content-type": "text/plain" },
-  });
-  event.respondWith(response);
-});
+
+// https://deno.com/deploy/docs/hello-world
+
+addEventListener("fetch", (event) => event.respondWith(handle(event.request)));
+
+function handle(request) {
+  let html = `<h1>Sudoku Solver Coming Soon</h1>`
+  return new Response(html, { headers: { "content-type": "text/html" } });
+}
