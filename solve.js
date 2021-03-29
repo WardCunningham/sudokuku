2e324f5dc0796ce474e506439f70428cfdb1b2b4
Ward Cunningham <ward@c2.com>
2021-03-29 14:01:59 -0700

    exploring

--- solve.js
+++ solve.js
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Sudoku Solver Coming Soon", {
+  const response = new Response(`Sudoku Solver Coming Soon\n\n${Object.keys(event).join("\n")}`, {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
