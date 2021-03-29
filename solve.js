21bbb77ac08883f5e73f1ec9a802f12f2a7cae1e
Ward Cunningham <ward@c2.com>
2021-03-29 14:43:57 -0700

    github link

--- solve.js
+++ solve.js
@@ -4,6 +4,7 @@
 addEventListener("fetch", (event) => event.respondWith(handle(event.request)));
 
 function handle(request) {
-  let html = `<h1>Sudoku Solver Coming Soon</h1>`
+  let html = `<h1>Sudoku Solver Coming Soon</h1>
+    Fork this on <a href="https://github.com/WardCunningham/sudokuku">github</a>`
   return new Response(html, { headers: { "content-type": "text/html" } });
 }
