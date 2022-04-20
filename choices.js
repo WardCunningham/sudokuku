c08c213ea1fd01bbdcb9cf56dccc87bc8f5fa9ef
Ward Cunningham <ward@c2.com>
2022-04-20 15:20:08 -0700

    simplify state

--- choices.js
+++ choices.js
@@ -5,46 +5,38 @@
 import * as rules from './rules.js'
 
 let query = rules.trim(Deno.args[0])
+query = '.3...1....24.3.5.....7.58...7...4.1.....8.7......579....6.5.3.7.4.......3...2..6.'
 query = '.475..9....1....4...24...15.3........527.418........9.27...13...6....2....1..457.'
 
-console.error('search', query)
+console.error(query)
+console.error('ones',todo(query, 1))
 
-let {givens,choices,unique} = rules.apply(query)
-console.error({givens,choices,unique})
-console.error('ones',todo(1))
+query = choice(query)
+console.error('ones',todo(query, 1))
+console.error('twos',todo(query, 2))
 
-
-const t0 = Date.now()
-nextChoice()
-console.error('time',Date.now() - t0)
-
-console.error('ones',todo(1))
-console.error('twos',todo(2))
-
-
-function nextForced() {
-  let res = givens.map((g,i) =>
-    g!='.' ? g :
-    choices[i].length==1 ? choices[i] :
-    unique[i] ? unique[i] : '.')
-  return res.join("")
+function force(query) {
+  const {givens,choices} = rules.apply(query)
+  return givens
+    .map((g,i) => g=='.' ? (choices[i].length==1 ? choices[i] : g) : g)
+    .join('')
 }
 
-function nextChoice() {
+function choice(query) {
   console.error('')
-  let res = givens.join("");
-  let state = rules.apply(res); givens = state.givens; choices = state.choices; unique = state.unique
-  console.error(res)
-  let nxt = nextForced();
-  while (nxt != res) {
-    res = nxt
-    state = rules.apply(res); givens = state.givens; choices = state.choices; unique = state.unique
-    console.error(res)
-    nxt = nextForced()
+  let here = query
+  console.error(here)
+  let there = force(here)
+  while (there != here) {
+    here = there
+    console.error(here)
+    there = force(here)
   }
+  return here
 }
 
-function todo(width) {
+function todo(query, width) {
+  const {givens,choices} = rules.apply(query)
   return givens
     .map((digit,i) => (digit == '.' && choices[i].length == width) ? {i,choice:choices[i]} : null)
     .filter(choice => choice != null)
